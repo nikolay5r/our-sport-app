400a9d7b5f7f951db1ae54b58f7f3adaa074ccc5
nikolay5r <nradilov30@gmail.com>
2021-12-09 22:27:36 +0200

    Edit Details component
    class -> className
    a href -> Link to

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,24 +1,30 @@
+import { Link } from 'react-router-dom'
+
 export default function Details() {
     return (
         <div className="page-content">
             <h1 className="heading">Details</h1>
             <div className="details">
                 <div className="post-info">
-                    <p>Title: Post title <br /> by: <a href="">Owner</a></p>
+                    <p>Title: Post title <br /> by: <Link to="/profile/userId">Owner</Link></p>
                     <p>Topic: Football</p>
                     <img src="./images/5e64d06674c6a.jpeg" alt="Post title" />
                     <p>Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quisquam
                         nisi autem hic
                         sapiente explicabo natus itaque voluptates fugit nostrum.</p>
                 </div>
-                <div className="likes">
-                    <a href="">Like</a>
-                    <a href="">Dislike</a>
+                <div className="attributes">
+                    {/* If not owner */}
+                    <Link to="/posts/postId/like">Like</Link>
+                    <Link to="/posts/postId/dislike">Dislike</Link>
+                    {/* If owner */}
+                    <Link to="/posts/postId/edit">Edit</Link>
+                    <Link to="/posts/postId/delete">Delete</Link>
                 </div>
                 <div className="comments">
                     <h3>Comments: *Number of comments*</h3>
                     <div className="comment">
-                        <a href="">Username</a>:
+                        <Link to="/profile/userId">Username</Link>:
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                             Eligendi ex magnam distinctio iusto quam similique architecto, eveniet dolor rem
                             nobis
@@ -28,7 +34,7 @@ export default function Details() {
                             dicta?</p>
                     </div>
                     <div className="comment">
-                        <a href="">Username</a>:
+                        <Link to="/profile/userId">Username</Link>:
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                             Eligendi ex magnam distinctio iusto quam similique architecto, eveniet dolor rem
                             nobis
