e064221e897ecb430b5d3f816a1c077f995881e6
nikolay5r <nradilov30@gmail.com>
2021-12-09 20:31:31 +0200

    Fix bug with my posts path

--- src/components/Navigation/UserNavigation.js
+++ src/components/Navigation/UserNavigation.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export default function UserNavigation() {
     return (
         <Fragment>
-            <NavLink className="nav-link" to="/my-posts">My posts</NavLink>
+            <NavLink className="nav-link" to="/posts/user/userId">My posts</NavLink>
             <NavLink className="nav-link" to="/posts/create">Create Post</NavLink>
             <NavLink className="nav-link" to="/profile/userId">My Profile</NavLink>
         </Fragment>
