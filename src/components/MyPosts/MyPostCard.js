7fb2b595f75501dea50dacfaf35520460e5ded4f
nikolay5r <nradilov30@gmail.com>
2021-12-09 20:33:58 +0200

    Extract MyPostCard

---
+++ src/components/MyPosts/MyPostCard.js
@@ -0,0 +1,11 @@
+export default function MyPostCard() {
+    return (
+        <div class="my-post-card">
+            <img src="./images/cover_image.jpg" alt="" />
+            <div class="info">
+                <p>Post name</p>
+                <a href="">View Details</a>
+            </div>
+        </div>
+    )
+}
--- src/components/MyPosts/MyPosts.js
+++ src/components/MyPosts/MyPosts.js
@@ -1,58 +1,17 @@
+import MyPostCard from "./MyPostCard";
+
 export default function MyPosts() {
     return (
         <div class="page-content">
             <div id="go-top"></div>
             <h1 class="heading">My Posts</h1>
             <div class="my-posts">
-                <div class="my-post-card">
-                    <img src="./images/cover_image.jpg" alt="" />
-                    <div class="info">
-                        <p>Post name</p>
-                        <a href="">View Details</a>
-                    </div>
-                </div>
-                <div class="my-post-card">
-                    <img src="./images/cover_image.jpg" alt="" />
-                    <div class="info">
-                        <p>Post name</p>
-                        <a href="">View Details</a>
-                    </div>
-                </div>
-                <div class="my-post-card">
-                    <img src="./images/cover_image.jpg" alt="" />
-                    <div class="info">
-                        <p>Post name</p>
-                        <a href="">View Details</a>
-                    </div>
-                </div>
-                <div class="my-post-card">
-                    <img src="./images/cover_image.jpg" alt="" />
-                    <div class="info">
-                        <p>Post name</p>
-                        <a href="">View Details</a>
-                    </div>
-                </div>
-                <div class="my-post-card">
-                    <img src="./images/cover_image.jpg" alt="" />
-                    <div class="info">
-                        <p>Post name</p>
-                        <a href="">View Details</a>
-                    </div>
-                </div>
-                <div class="my-post-card">
-                    <img src="./images/cover_image.jpg" alt="" />
-                    <div class="info">
-                        <p>Post name</p>
-                        <a href="">View Details</a>
-                    </div>
-                </div>
-                <div class="my-post-card">
-                    <img src="./images/cover_image.jpg" alt="" />
-                    <div class="info">
-                        <p>Post name</p>
-                        <a href="">View Details</a>
-                    </div>
-                </div>
+                
+                <MyPostCard />
+                <MyPostCard />
+                <MyPostCard />
+                <MyPostCard />
+
             </div>
         </div>
     )
