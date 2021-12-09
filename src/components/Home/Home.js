d64dbf262d855b1be12bd9daf7ff0674a6abc6df
nikolay5r <nradilov30@gmail.com>
2021-12-09 19:15:39 +0200

    Extract welcome text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,11 @@
 import { Fragment } from "react";
-import PopularTopicCard from "./PopularTopicCard";
 import PopularTopics from './PopularTopics';
+import Welcome from "./Welcome";
 
 export default function Home() {
     return (
         <Fragment>
-            <div className="welcome">
-                <p className="welcome-text">Welcome to<br />
-                    <span className="our-sport">Our Sport</span>
-                </p>
-            </div>
+            <Welcome />
             <hr />
             <PopularTopics />
         </Fragment>
---
+++ src/components/Home/Welcome.js
@@ -0,0 +1,9 @@
+export default function Welcome() {
+    return (
+        <div className="welcome">
+            <p className="welcome-text">Welcome to<br />
+                <span className="our-sport">Our Sport</span>
+            </p>
+        </div>
+    )
+}
