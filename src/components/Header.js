07f08cbdef5d0b98ab2856f2ff15a01c2bcd854f
nikolay5r <nradilov30@gmail.com>
2021-12-09 17:35:29 +0200

    Change navigation (header) paths
    Change href in header from '#' to actual paths

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,16 @@
 export default function Header() {
     return (
         <header className="navigation">
-            <a className="logo our-sport" href="#">Our Sport</a>
+            <a className="logo our-sport" href="/">Our Sport</a>
             <div className="right">
-                <a className="nav-link" href="#">All Posts</a>
+                <a className="nav-link" href="/posts">All Posts</a>
                 {/*  User */}
-                <a className="nav-link" href="#">My posts</a>
-                <a className="nav-link" href="#">Create Post</a>
-                <a className="nav-link" href="#">My Profile</a>
+                <a className="nav-link" href="/my-posts">My posts</a>
+                <a className="nav-link" href="/posts/create">Create Post</a>
+                <a className="nav-link" href="/profile/userId">My Profile</a>
                 {/* Guest */}
-                <a className="nav-link" href="#">Login</a>
-                <a className="nav-link" href="#">Register</a>
+                <a className="nav-link" href="/login">Login</a>
+                <a className="nav-link" href="/register">Register</a>
             </div>
         </header>
     )
