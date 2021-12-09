b3c3e0ddd82c889fe5abf233d81e07a9c5ccde5e
nikolay5r <nradilov30@gmail.com>
2021-12-09 20:47:37 +0200

    Extract content to PostCardContent

--- src/components/AllPosts/PostCard.js
+++ src/components/AllPosts/PostCard.js
@@ -1,21 +1,11 @@
-import { Link } from 'react-router-dom';
+import PostCardContent from './PostCardContent';
 import PostCardOwner from './PostCardOwner';
 
 export default function PostCard() {
     return (
         <div className="post-card">
             <PostCardOwner />
-            <div className="post-content">
-                <img src="./images/generic-basketball-shot.jpg" alt="Post Title" className="post-image" />
-                <p className="description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, quisquam sequi pariatur
-                    quidem natus veritatis obcaecati quia modi odit quam, magni omnis!
-                    Asperiores, assumenda expedita dolores libero inventore eos commodi. Lorem ipsum dolor sit amet ...
-                </p>
-                <div className="post-elements">
-                    <Link to="/posts/postId" className="post-element">View Details</Link>
-                </div>
-            </div>
+            <PostCardContent />
         </div>
     )
 }
---
+++ src/components/AllPosts/PostCardContent.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+export default function PostCardContent() {
+    return (
+        <div className="post-content">
+            <img src="./images/generic-basketball-shot.jpg" alt="Post Title" className="post-image" />
+            <p className="description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, quisquam sequi pariatur
+                quidem natus veritatis obcaecati quia modi odit quam, magni omnis!
+                Asperiores, assumenda expedita dolores libero inventore eos commodi. Lorem ipsum dolor sit amet ...
+            </p>
+            <div className="post-elements">
+                <Link to="/posts/postId" className="post-element">View Details</Link>
+            </div>
+        </div>
+    )
+}
